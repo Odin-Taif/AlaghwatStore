9e59c98b775f4455330e559e1ecceff9122d7ed2
Odin Taif <mjd.thif@icloud.com>
2022-12-29 13:23:03 +0100

    initial

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ function IndexPage(props) {
   return (
     <div className="my-8">
       <div className="mt-4">
-        <ProductsPage products={products} />k
+        <ProductsPage products={products} />
       </div>
     </div>
   );
